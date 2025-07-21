1fdcb40904b0413b5c928fb74e84860673222312
mikechao <mike.chao.one@gmail.com>
2025-07-21 12:00:24 -0700

    Added count

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <h1>This is the index page</h1>
+    <div>
+      <h2>Current Count: {{ count }}</h2>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+// Access the same global state defined in app.vue
+const count = useState('count', () => 0)
 
 </script>
